b26dec115710b232ed78cbc37aa773168ede4d08
AmoutzidisKostas <139139176+AmoutzidisKostas@users.noreply.github.com>
2023-07-11 15:33:28 +0300

    Update ping_pong.py
    window creation

--- ping_pong.py
+++ ping_pong.py
@@ -1,2 +1,25 @@
 from pygame import *
-ds
+
+
+
+#window settings
+back = (200,255,255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_height,win_width))
+window.fill(back)
+
+clock = time.Clock()
+FPS = 60
+game = True
+
+
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    
+
+    display.update 
+    clock.tick(FPS)
